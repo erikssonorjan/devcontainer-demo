f3ffd802c844f593b98350388465e23390f0ad44
Örjan Eriksson <orjan.eriksson@squeed.com>
2020-12-02 11:24:07 +0100

    Inital commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 4321;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('God Jul');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Surfa till http://${hostname}:${port}`);
+});
